46c97e7c28cfa93dbb093383ff8fc8cc69e0b7a0
miloszzaj <zajaczkowski.milosz@gmail.com>
2021-03-06 21:13:11 +0100

    not valid form added

--- contactForm.js
+++ contactForm.js
@@ -2,59 +2,24 @@
 const submitBtn = document.querySelector('.submitBtn')
 
 const email = document.querySelector('.email')
+const statusEmail = document.querySelector('.statusEmail')
+
 const clientName = document.querySelector('.name')
+const statusName = document.querySelector('.statusName')
 
 const sendContactForm = function(e) {
-    if(email.length >5 && email.includes('@') && email.includes('.')){
-        e.preventDefault()
-        // console.log("działa")
-        email.classList.remove("notValid")
+    e.preventDefault()
+    if(email.value.length >5 && email.value.includes('@') && email.value.includes('.')){
+        statusEmail.classList.remove("notValid")
     }
     else {
-        // console.log('nie działa')
-        email.classList.add("notValid")
+        statusEmail.classList.add("notValid")
     }
-    if(clientName.length >=2){
-        e.preventDefault()
-        console.log("działa")
-        clientName.classList.remove("notValid")
+    if(clientName.value.length >=2){
+        statusName.classList.remove("notValid")
     }
     else {
-        console.log('nie działa')
-        clientName.classList.add("notValid")
+        statusName.classList.add("notValid")
     }
 }
 submitBtn.addEventListener('click', sendContactForm)
-
-
-// $(document).ready(function() {
-// 	$('.submitBtn').click(function(event) {
-// console.log('clicked button')
-
-// const email = $('.email').val()
-// const name = $('.name').val()
-// const message = $('.messageSubmit').val()
-// const statusElm = $('.status').val()
-// statusElm.empty()
-
-// if(email.length >5 && email.includes('@') && email.includes('.')){
-//     statusElm.classList.add("notValid")
-// } else {
-//     // event.preventDefault()
-//     // statusElm.append('<div>email is not valid</div>')
-// }
-
-// if(name.length>=2) {
-//     statusElm.append('<div>name is valid</div>')
-// } else {
-//     event.preventDefault()
-//     statusElm.append('<div>name is not valid</div>')
-// }
-
-// if(message.length>10) {
-//     statusElm.append('<div>message is valid</div>')
-// } else {
-//     event.preventDefault()
-//     statusElm.append('<div>message is not valid</div>')
-// }
-// 	})}
