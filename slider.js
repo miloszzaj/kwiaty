1ead67fd7f0876ae3f60a5f151f0cbe8847c190d
miloszzaj <zajaczkowski.milosz@gmail.com>
2021-03-12 20:48:38 +0100

    slider setTimeout

--- slider.js
+++ slider.js
@@ -1,27 +1,33 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
+
+var slideIndex = 0;
+showSlides();
+
+function plusSlide(n) {
   showSlides(slideIndex += n);
+  // clearTimeout(indexTimeout)
+
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
-}
-// let timer = setInterval(showSlides, 2000)
 
-function showSlides(n) {
+}
+
+
+function showSlides() {
+  // let indexTimeout = setTimeout(plusSlide, 2000)
   let i;
   const slides = document.getElementsByClassName("slides");
-  const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  indexTimeout = setTimeout(plusSlide, 2000);
 }
