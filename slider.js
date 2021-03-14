eec12860759f54a654e62e299ea15ddeb997ca1f
miloszzaj <zajaczkowski.milosz@gmail.com>
2021-03-14 21:12:00 +0100

    media query

--- slider.js
+++ slider.js
@@ -3,23 +3,24 @@
 var slideIndex = 0;
 showSlides();
 
-function plusSlide(n) {
-  showSlides(slideIndex += n);
-  // clearTimeout(indexTimeout)
+// function plusSlide(n) {
+//   showSlides(slideIndex += n);
+//   // clearTimeout(indexTimeout)
 
-}
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
 
-}
+// }
 
 
 function showSlides() {
   // let indexTimeout = setTimeout(plusSlide, 2000)
   let i;
   const slides = document.getElementsByClassName("slides");
+  // const dots
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -28,6 +29,6 @@ function showSlides() {
     slideIndex = 1
   }
 
-  slides[slideIndex-1].style.display = "block";
-  indexTimeout = setTimeout(plusSlide, 2000);
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 2000);
 }
